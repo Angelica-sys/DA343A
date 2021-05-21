9557fdcc73a356fd4699de4b501def2303b759ce
ludd3p <77840976+ludd3p@users.noreply.github.com>
2021-05-21 14:15:04 +0200

    Testar uppkoppling

--- src/Ludvig/TestGUI.java
+++ src/Ludvig/TestGUI.java
@@ -4,6 +4,7 @@ import Ludvig.Client.View.MainView;
 
 public class TestGUI {
     public static void main(String[] args) {
-        new ClientController("127.0.0.1", 23);
+        new ClientController("127.0.0.1", 55234);
+        new ClientController("127.0.0.1", 55234);
     }
 }
